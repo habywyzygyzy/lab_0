4e4d1661c71b82f6f71e8cc637c239b646c553ca
Błażejczuk.Kamil <202119@edu.p.lodz.pl>
2018-02-27 17:49:55 +0100

    Fix TailsGenerator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        ArrayList <String> temp = new ArrayList<>();
+        temp.add(value);
+        for (int i = 1; i < value.length(); i++){
+            temp.add(temp.get(i-i).substring(1));
+        }
+        temp.add("");
+        return temp;
     }
 
 }
