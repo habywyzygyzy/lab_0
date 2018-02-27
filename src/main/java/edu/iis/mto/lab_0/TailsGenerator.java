5ea62d30939d52795e7ebc17289af710d69db3f6
Błażejczuk.Kamil <202119@edu.p.lodz.pl>
2018-02-27 18:12:08 +0100

    Tails Generator commit

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,12 +1,19 @@
 package edu.iis.mto.lab_0;
 
-import java.util.ArrayList;
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        ArrayList <String> temp = new ArrayList<>();
+        temp.add(value);
+        for (int i = 1; i < value.length(); i++){
+            temp.add(temp.get(i-i).substring(1));
+        }
+        temp.add("");
+        return temp;
     }
 
 }
